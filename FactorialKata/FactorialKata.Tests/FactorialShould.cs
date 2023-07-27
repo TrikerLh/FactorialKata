4250dd343200cfcb3fb987cd05c695f38fe61fcc
Raúl Corchero <corchero100@gmail.com>
2023-07-27 07:11:27 +0100

    green: Return_factorial

--- FactorialKata/FactorialKata.Tests/FactorialShould.cs
+++ FactorialKata/FactorialKata.Tests/FactorialShould.cs
@@ -1,11 +1,16 @@
 using FluentAssertions;
 
-namespace FactorialKata.Tests {
-    public class FactorialShould {
+namespace FactorialKata.Tests
+{
+    public class FactorialShould
+    {
         [TestCase(0, 1)]
         [TestCase(2, 2)]
         [TestCase(3, 6)]
-        public void Return_factorial(int number, int expected) {
+        [TestCase(4, 24)]
+        [TestCase(5, 120)]
+        public void Return_factorial(int number, int expected)
+        {
             var result = Factorial.Get(number);
 
             result.Should().Be(expected);
--- FactorialKata/FactorialKata/Factorial.cs
+++ FactorialKata/FactorialKata/Factorial.cs
@@ -5,7 +5,12 @@ public class Factorial
     public static int Get(int number)
     {
         if(number == 0) return 1;
-        if(number == 3) return 6;
-        return number;
+        var result = number * (number - 1);
+        for (var i = number - 1; i > 1; i--)
+        {
+            result *= (i - 1);
+        }
+
+        return result;
     }
 }

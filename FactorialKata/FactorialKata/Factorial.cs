b9a715d94793f0395f195eb0f35a64642040df5b
Raúl Corchero <corchero100@gmail.com>
2023-07-27 07:37:20 +0100

    refactor with recursive

--- FactorialKata/FactorialKata/Factorial.cs
+++ FactorialKata/FactorialKata/Factorial.cs
@@ -5,12 +5,6 @@ public class Factorial
     public static int Get(int number)
     {
         if(number == 0) return 1;
-        var result = number * (number - 1);
-        for (var i = number - 1; i > 1; i--)
-        {
-            result *= (i - 1);
-        }
-
-        return result;
+        return number * Get(number - 1);
     }
 }
